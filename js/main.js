d85d3f031cbf763791a926c2208f3f7bbd6d9aaa
alokuiux <alok.ranjan@ofcom.org.uk>
2023-04-28 07:40:29 +0100

    add back to top and skip containt

--- js/main.js
+++ js/main.js
@@ -268,3 +268,18 @@ $(document).ready(function () {
   html += '</table>';
   $('#tableData').html(html);
 });
+
+var btn = $('.back-to-top');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
